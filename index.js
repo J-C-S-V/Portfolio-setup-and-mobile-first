ccdcda5f7f0c729754fca3711d7756a5473df930
Juan Sanchez <juansan1993@hotmail.com>
2023-03-06 18:17:41 -0500

    Update comments on JS

--- index.js
+++ index.js
@@ -1,21 +1,42 @@
 let header = document.querySelector('header');
 let body = document.querySelector('body');
 let popUpMenuDiv = document.createElement('div');
-body.style.filter = 'blur(3px)';
+let unorderedList = document.createElement('ul');
+
 body.insertBefore(popUpMenuDiv, header);
+popUpMenuDiv.appendChild(unorderedList);
+
+//////////////////
+
+const titlesOfMenu = ['X', 'Portfolio', 'About', 'Contact'];
+
+for (let i = 0; i < titlesOfMenu.length; i++) {
+  let listElement = document.createElement('li');
+  listElement.innerText = titlesOfMenu[i];
+  unorderedList.appendChild(listElement);
+}
+
+/////////////////
+// let listElements = document.createElement('li');
+// let listElements2 = document.createElement('li');
+// let listElements3 = document.createElement('li');
+// let listElements4 = document.createElement('li');
+// unorderedList.appendChild(listElements);
+// unorderedList.appendChild(listElements2);
+// unorderedList.appendChild(listElements3);
+// unorderedList.appendChild(listElements4);
+
+body.style.filter = 'blur(3px)';
 // console.log(body);
 // popUpMenuDiv.innerText = 'hey!';
 popUpMenuDiv.style.position = 'fixed';
 popUpMenuDiv.style.width = '100%';
+popUpMenuDiv.style.height = '100vh';
 popUpMenuDiv.style.backgroundColor = '#6070FF';
 popUpMenuDiv.style.mixBlendMode = 'multiply';
-popUpMenuDiv.style.height = '100vh';
 // popUpMenuDiv.style.opacity = '0.9';
 
 // console.log(popUpMenuDiv);
-let unorderedList = document.createElement('ul');
-
-popUpMenuDiv.appendChild(unorderedList);
 
 {
   /* <div class="popUpMenuDiv">
@@ -32,3 +53,7 @@ popUpMenuDiv.appendChild(unorderedList);
 // width: 100%;
 // background: yellow;
 // height: 100vh;
+
+header - bar;
+main - section;
+main - container - projects;
