bf1dacbfad59c99fbf2a92f39b255453ffbff4f4
Juan Sanchez <juansan1993@hotmail.com>
2023-03-06 20:02:54 -0500

    Update white color

--- index.js
+++ index.js
@@ -3,17 +3,26 @@ let body = document.querySelector('body');
 let popUpMenuDiv = document.createElement('div');
 let unorderedList = document.createElement('ul');
 
+let headerBar = document.querySelector('.header-bar');
+let mainSection = document.querySelector('.main-section');
+let mainContainerProjects = document.querySelector('.main-container-projects');
+
+headerBar.style.filter = 'blur(6px)';
+mainSection.style.filter = 'blur(6px)';
+mainContainerProjects.style.filter = 'blur(6px)';
+
 body.insertBefore(popUpMenuDiv, header);
 popUpMenuDiv.appendChild(unorderedList);
 
 //////////////////
 
-const titlesOfMenu = ['X', 'Portfolio', 'About', 'Contact'];
+const titlesOfMenu = ['Portfolio', 'About', 'Contact'];
 
 for (let i = 0; i < titlesOfMenu.length; i++) {
   let listElement = document.createElement('li');
   listElement.innerText = titlesOfMenu[i];
   unorderedList.appendChild(listElement);
+  listElement.style.color = 'white';
 }
 
 /////////////////
@@ -26,10 +35,13 @@ for (let i = 0; i < titlesOfMenu.length; i++) {
 // unorderedList.appendChild(listElements3);
 // unorderedList.appendChild(listElements4);
 
-body.style.filter = 'blur(3px)';
+// body.style.filter = 'blur(3px)';
+
+console.log(body.length);
+
 // console.log(body);
 // popUpMenuDiv.innerText = 'hey!';
-popUpMenuDiv.style.position = 'fixed';
+popUpMenuDiv.style.position = 'absolute';
 popUpMenuDiv.style.width = '100%';
 popUpMenuDiv.style.height = '100vh';
 popUpMenuDiv.style.backgroundColor = '#6070FF';
@@ -54,6 +66,6 @@ popUpMenuDiv.style.mixBlendMode = 'multiply';
 // background: yellow;
 // height: 100vh;
 
-header - bar;
-main - section;
-main - container - projects;
+// header - bar;
+// main - section;
+// main - container - projects;
