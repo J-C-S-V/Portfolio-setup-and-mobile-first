3043753db0647dad94bb6bbec708137a709ab7b0
Juan Sanchez <juansan1993@hotmail.com>
2023-03-06 20:43:59 -0500

    Update menu options style

--- index.js
+++ index.js
@@ -2,10 +2,31 @@ let header = document.querySelector('header');
 let body = document.querySelector('body');
 let popUpMenuDiv = document.createElement('div');
 let unorderedList = document.createElement('ul');
+let crossIcon = document.createElement('a');
 
 let headerBar = document.querySelector('.header-bar');
 let mainSection = document.querySelector('.main-section');
 let mainContainerProjects = document.querySelector('.main-container-projects');
+
+// Cross Icon styling
+
+crossIcon.innerText = 'X';
+crossIcon.style.color = 'white';
+crossIcon.style.fontSize = '2rem';
+crossIcon.style.position = 'absolute';
+crossIcon.style.top = '1.5rem';
+crossIcon.style.right = '1rem';
+crossIcon.style.padding = '1rem';
+crossIcon.style.cursor = 'pointer';
+
+// popUpMenuDiv styling
+
+popUpMenuDiv.style.fontWeight = '600';
+popUpMenuDiv.style.fontSize = '32px';
+popUpMenuDiv.style.display = 'flex';
+popUpMenuDiv.style.padding = '7.6rem 1.5rem';
+
+popUpMenuDiv.appendChild(crossIcon);
 
 headerBar.style.filter = 'blur(6px)';
 mainSection.style.filter = 'blur(6px)';
@@ -20,8 +41,10 @@ const titlesOfMenu = ['Portfolio', 'About', 'Contact'];
 
 for (let i = 0; i < titlesOfMenu.length; i++) {
   let listElement = document.createElement('li');
-  listElement.innerText = titlesOfMenu[i];
+  let listElementAnchor = document.createElement('a');
   unorderedList.appendChild(listElement);
+  listElement.appendChild(listElementAnchor);
+  listElementAnchor.innerText = titlesOfMenu[i];
   listElement.style.color = 'white';
 }
 
@@ -66,6 +89,13 @@ popUpMenuDiv.style.mixBlendMode = 'multiply';
 // background: yellow;
 // height: 100vh;
 
+/* DEV TOOLS PROPERTIES */
+
 // header - bar;
 // main - section;
 // main - container - projects;
+
+// font-weight: 600;
+// font-size: 32px;
+// display: flex;
+// padding: 7.6rem 1.5rem;
